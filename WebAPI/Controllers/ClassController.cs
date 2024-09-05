8a04ad563f56017ce93b1abaaa45cc51f72dbf1f
呂璇 <teapudding8710@gmail.com>
2024-09-05 19:06:55 +0800

    Modify:調整classController的create，改用(distributed) lock

--- WebAPI/Controllers/ClassController.cs
+++ WebAPI/Controllers/ClassController.cs
@@ -6,14 +6,38 @@ using WebAPI.Models.Class;
 using Common;
 using WebAPIBasic.Models.Shared;
 using WebAPI.Models.Student;
+using DistributedLock;
 
 namespace WebAPI.Controllers
 {
+
+    /*
     [Route("[controller]/[action]")]
     [ApiController]
     public class ClassController(DatabaseContext db , IdService idService) : ControllerBase
     {
-        //IdService idService = 要做數據的插入才需要這個
+
+    */
+    //IdService idService = 要做數據的插入才需要這個
+
+
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class classController : ControllerBase
+    {
+        private readonly DatabaseContext db;
+        private readonly IdService idService;
+
+        private readonly IDistributedLock distributedLock;
+
+        public classController(DatabaseContext _db, IdService _idService, IHttpContextAccessor httpContextAccessor, IDistributedLock distributedLock)
+        {
+            db = _db;  
+            idService = _idService;
+            this.distributedLock = distributedLock;
+        }
+
+
 
 
         //獲取(查詢)班級訊息的一個API
@@ -67,35 +91,94 @@ namespace WebAPI.Controllers
         }
 
 
+
+        //共三種方式:內存、lock、distributedlock
+
+        private static object checkClassName = "";
+
+        //新增數據到資料庫--distributedLock--[高併發下使用分佈式鎖]
+        [HttpPost]
+        public long CreateClass(DtoCreateClass request)
+        {
+            //將原本存在內存的值，抽出去到redis儲存(不管啟動幾個，redis都保持一個)
+            //就可以在多個程序間共享redis的值
+            using (var handle = distributedLock.TryLock("checkClassName"))
+            {
+                //using:在跑完using內部的程式之後，會自動釋放鎖定
+                //TryLock:嘗試看看有沒有鎖住，傳值給handle，如果handle有值，就執行---使其他沒有搶到位置的，直接離開
+                //Lock:其他沒有等到位置的，要繼續排隊等待，所以花費時間比較久
+
+
+                if (handle != null)
+                {
+                    var isHave = db.TClass.Where(t => t.Name == request.Name).Any();
+
+                    if (!isHave)
+                    {
+                        TClass Class = new()
+                        {
+                            Id = idService.GetId(),
+                            Name = request.Name,
+                            Grade = request.Grade,
+                        };
+                        db.TClass.Add(Class);
+                        db.SaveChanges();
+                        return Class.Id;
+                    }
+                    else
+                    {
+                        throw new CustomException("班級已存在");
+                    }
+                }
+                else
+                {
+                    throw new CustomException("班級已存在");
+                }
+            }
+
+        }
+
+
+
+        /*
+        
+        //static靜態:程式啟動後就只有new一次，不會一直重複
+        //不管有多少請求，這個在內存中只有一份      
+        private static object checkClassName = "";
+        //搭配lock (checkClassName)使用
+        //lock只能用在鎖定一個程序中的資源，但如果有多台API或伺服器或相同程序在執行，就無法只鎖定一個
+
+
         //新增數據到資料庫
         [HttpPost]  //提交數據到伺服器
         public long CreateClass(DtoCreateClass request)//用這個方法(DtoCreateClass)傳入參數(request)
         {
-            //判斷資料庫中Name是否已經存在
-            var isHave = db.TClass.Where(t => t.Name == request.Name).Any();
-
-            if (!isHave)
+            //lock:所有請求一次過來，但lock只會提供一個位置給一個請求做
+            lock (checkClassName)
             {
-                TClass Class = new()
+                //判斷資料庫中Name是否已經存在
+                var isHave = db.TClass.Where(t => t.Name == request.Name).Any();
+
+                if (!isHave)
                 {
-                    Id = idService.GetId(),
-                    Name = request.Name,
-                    Grade = request.Grade,
-                };
-
-                db.TClass.Add(Class);
-
-                db.SaveChanges();
-
-                return Class.Id;
-            }
-            else
-            {
-                throw new CustomException("班級已存在");
+                    TClass Class = new()
+                    {
+                        Id = idService.GetId(),
+                        Name = request.Name,
+                        Grade = request.Grade,
+                    };
+                    db.TClass.Add(Class);
+                    db.SaveChanges();
+                    return Class.Id;
+                }
+                else
+                {
+                    throw new CustomException("班級已存在");
+                }
             }
         }
 
-
+        */
 
 
 
